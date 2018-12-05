39c4c232b09dcf4c0216be74ee44c81578e76e4a
Samuel Pedro <samuelcpedro@gmail.com>
2018-12-05 12:05:53 -0100

    When starting the game, an event (holding a incrementing number) should get emitted each second (ref = setInterval())

--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameControlComponent implements OnInit {
 
+  private num = 0;
   constructor() { }
 
   ngOnInit() {
   }
 
+  startGame() {
+    setInterval(() => {
+      ++this.num;
+      console.log(this.num);
+    }, 1000);
+  }
 }
