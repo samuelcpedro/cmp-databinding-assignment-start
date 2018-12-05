9dbd2eca0d9207cfc0f10bb807bb0572b0de28c8
Samuel Pedro <samuelcpedro@gmail.com>
2018-12-05 20:30:10 -0100

    A new Odd component should get created for every odd number emitted, the same should happen for the Even Component (on even numbers)
    A new Odd component should get created for every odd number emitted, the same should happen for the Even Component (on even numbers)
    Simply output Odd - NUMBER or Even - NUMBER in the two components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  // for test purpose, will show on the page
   numX: number;
 
+  numArr: Array<{even: boolean, numb: number}> = [];
+
+
+  // can not be initialized inside,
+  // it will show only one number at a time
+  // button start must be disabled in the component
   onStartBtClick(num: number) {
+
+    this.numX = num;
+
     // console.log('2 - Before num in AppComponent');
     // console.log(num);
-    this.numX = num;
+    this.numArr.push({
+      'even': (num % 2 === 0) ? true : false,
+      'numb': num
+    });
     // console.log('3 - After num in AppComponent');
     // console.log(num);
   }
